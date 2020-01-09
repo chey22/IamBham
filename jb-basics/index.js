729ac1610ca28d8b236bc3180fa8f87bf064518a
chey22 <cheyennemangum@gmail.com>
2020-01-09 10:02:07 -0600

    First commit

---
+++ jb-basics/index.js
@@ -0,0 +1,6 @@
+       // comment (meaning just for reference or to explain WHY/HOW you wrote the code this way. NOT executed )
+       console.log('Hello World')
+       var numberOfLines =  'lines of code';
+       var number = 99;
+       console.log(number +' '+numberOfLines);
+       console.log(number -= 1+' '+numberOfLines)
